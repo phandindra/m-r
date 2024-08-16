c87a432686fbe9dfd438c2cdf9d3e5b8ca411f34
aditya <adityaedu22@gmail.com>
2024-08-16 15:32:43 +0530

    back upd

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+app.use(cors(`*`));
+
 const prisma = new PrismaClient();
 const app = express();
 
@@ -25,12 +28,16 @@ app.get('/movies', async (req, res) => {
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const movie = await prisma.movie.findUnique({
-      where: { id: parseInt(id) },
-      include: {
-        reviews: true,
-      },
-    });
+    const movies = await prisma.movie.findMany({
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          image: true,  
+          reviews: true,  
+        },
+      });
+      res.json(movies);
     if (!movie) {
       return res.status(404).json({ error: 'Movie not found' });
     }
@@ -40,18 +47,18 @@ app.get('/movies/:id', async (req, res) => {
   }
 });
 
-// Add a new movie
-app.post('/movies', async (req, res) => {
-  const { title, description } = req.body;
-  try {
-    const newMovie = await prisma.movie.create({
-      data: { title, description },
-    });
-    res.status(201).json(newMovie);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// // Add a new movie
+// app.post('/movies', async (req, res) => {
+//   const { title, description } = req.body;
+//   try {
+//     const newMovie = await prisma.movie.create({
+//       data: { title, description },
+//     });
+//     res.status(201).json(newMovie);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 // Add a new review to a movie
 app.post('/movies/:id/reviews', async (req, res) => {
@@ -72,19 +79,19 @@ app.post('/movies/:id/reviews', async (req, res) => {
 });
 
 // Edit a review
-app.put('/reviews/:id', async (req, res) => {
-  const { id } = req.params;
-  const { content } = req.body;
-  try {
-    const updatedReview = await prisma.review.update({
-      where: { id: parseInt(id) },
-      data: { content },
-    });
-    res.json(updatedReview);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// app.put('/reviews/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const { content } = req.body;
+//   try {
+//     const updatedReview = await prisma.review.update({
+//       where: { id: parseInt(id) },
+//       data: { content },
+//     });
+//     res.json(updatedReview);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 // Delete a review
 app.delete('/reviews/:id', async (req, res) => {
